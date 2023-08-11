dda13ad8a34c1f019e6eaaa7ca2b60f7cf429d3b
omiko-dev <omikovalishvili0@gmail.com>
2023-08-11 23:57:01 +0400

    update profile page

---
+++ src/app/Dto/CartProduct.ts
@@ -0,0 +1,18 @@
+export type CartProduct = {
+  allergens: number[];
+  allergensJson: string;
+  brand: string;
+  cocoaPercentage: number;
+  createdAt: string;
+  description: string;
+  flavor: 0 | 1 | 2;
+  id: number;
+  imageURL: string[];
+  imageURlJson: string;
+  ingredients: string[];
+  ingredientsJson: string;
+  price: number;
+  productName: string;
+  updatedAt: string;
+  userid: number;
+};
---
+++ src/app/pages/profile-page/profile-product-info/profile-product-purchase/profile-product-purchase.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { catchError, map } from 'rxjs';
+import { UserService } from 'src/app/Services/user.service';
+
+@Component({
+  selector: 'app-profile-product-purchase',
+  templateUrl: './profile-product-purchase.component.html',
+  styleUrls: ['./profile-product-purchase.component.scss'],
+})
+export class ProfileProductPurchaseComponent implements OnInit {
+  constructor(private readonly userService: UserService) {}
+
+  data: any;
+  ngOnInit(): void {
+    var token = localStorage.getItem('token');
+    this.data = this.userService.getUser(token!).pipe(
+      map((data) => {
+        console.log(data);
+        return data.cart;
+      }),
+      catchError((err) => err)
+    );
+  }
+}
